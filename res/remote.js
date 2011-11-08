8f2ccb0c010efb293c68872530f9b12950aa3ece
Alex Ciminian <alex.ciminian@gmail.com>
2011-11-09 01:01:10 +0200

    improved view and added remote control scripts

---
+++ res/remote.js
@@ -0,0 +1,25 @@
+var socket = io.connect('http://fierce-dawn-8677.herokuapp.com/');
+socket.on('connect', function () {
+    socket.emit('remote', 'user01');
+    console.log('remote control connected');
+});
+
+socket.on('reconnecting', function () {
+    console.log('Reconnecting to the server');
+});
+
+socket.on('error', function (e) {
+    console.log(e, "An error occured");
+});
+
+$(document).ready(function () {
+    var next = $('#next'), prev = $('#prev');
+
+    next.live('tap', function () {
+        socket.emit('next');
+    });
+
+    prev.live('tap', function () {
+        socket.emit('prev');
+    });
+});
